a807a8c0c30489a79f59b814285467396945bfb1
Erik <Erik.quintana@icloud.com>
2022-05-17 09:42:28 -0600

    commit tree 2f155c12cf956e5383b37fdaefaabf56fd27589f

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(amount){
+  let message
+  if (amount <= 400) {
+    message = "This one is on me!";
+  } 
+  else if (amount > 400 && amount < 2000 ){
+    message = 'That will be twenty bucks.'
+  }
+  else if(amount > 2000 && amount < 2500){
+    message = 'I will gladly take your thirty bucks.'
+  }
+  else{ 
+    message = 'No can do.';
+  }
+  return message 
+
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
+function ternaryCheckCity(city){
+ let message 
+ city === "NYC" ? (message = 'Ok, sounds good.') : (message = 'No go.');
+ 
+ return message 
 }
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let message 
+  switch (tip){
+    case 'generous':
+      message = "Thank you so much.";
+      break;
+    case "not as generous" :
+      message = 'Thank you.'
+      break;
+    default:
+      message = "Bye."
+      break;
+  }
+  return message 
+ 
 }
